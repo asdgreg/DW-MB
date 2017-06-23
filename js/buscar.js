42e674dc60331fd801333f564fdf0da9bbd52016
Christian Sauceda <asdgreg@hotmail.com>
2017-06-22 23:34:49 -0600

    Add files via upload

---
+++ js/buscar.js
@@ -0,0 +1,18 @@
+function verificacion(){
+	var error = false;
+
+	if(!document.getElementById("txt-buscar").value){
+		error = true;
+		document.getElementById("txt-buscar").style.borderColor = "#FF0000";
+	}else{
+		document.getElementById("txt-buscar").style.borderColor = "#ccc";
+	}
+
+	if(error){
+		console.log("ERROR");
+		document.getElementById("mensajes").innerHTML = "<p class='text-danger'>Ingrese texto en la busqueda</p>";
+	}else{
+		document.getElementById("mensajes").innerHTML = "<p class='text-success'>Buscando:'"+ document.getElementById("txt-buscar").value +"'</p>";
+	}
+
+}
--- js/registro.js
+++ js/registro.js
@@ -1,44 +1,23 @@
 function verificacion(){
 	var error = false;
 
-	if(!document.getElementById("txt-nombre").value){
+	if(!document.getElementById("txt-buscar").value){
 		error = true;
-		document.getElementById("txt-nombre").style.borderColor = "#FF0000";
+		document.getElementById("txt-buscar").style.borderColor = "#FF0000";
 	}else{
-		document.getElementById("txt-nombre").style.borderColor = "#ccc";
-	}
-
-	if(!document.getElementById("txt-usuario").value){
-		error = true;
-		document.getElementById("txt-usuario").style.borderColor = "#FF0000";
-	}else{
-		document.getElementById("txt-usuario").style.borderColor = "#ccc";
-	}
-
-
-
-	if(!document.getElementById("txt-pass").value){
-		error = true;
-		document.getElementById("txt-pass").style.borderColor = "#FF0000";
-	}else{
-		document.getElementById("txt-pass").style.borderColor = "#ccc";
-	}
-
-	if(!document.getElementById("txt-verpass").value){
-		error = true;
-		document.getElementById("txt-verpass").style.borderColor = "#FF0000";
-	}else{
-		document.getElementById("txt-verpass").style.borderColor = "#ccc";
+		document.getElementById("txt-buscar").style.borderColor = "#ccc";
 	}
 
 	if(error){
 		console.log("ERROR");
+		document.getElementById("mensajes").innerHTML = "<p class='text-danger'>Ingrese texto en la busqueda</p>";
 	}else{
+		document.getElementById("mensajes").innerHTML = "<p class='text-danger'>Buscando:"+ document.getElementById("txt-buscar").value +"</p>";
 		console.log("Entra");
 		var pass1 = document.getElementById("txt-pass").value;
 		var pass2 = document.getElementById("txt-verpass").value;
 		if(pass1 != pass2){
-			document.getElementById("mensajes").innerHTML = "<p class='text-danger'>No coinciden las Contrasenas</p>";
+			
 			console.log("distintos");
 			document.getElementById("txt-verpass").style.borderColor = "#FF0000";
 			document.getElementById("txt-pass").style.borderColor = "#FF0000";
